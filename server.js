d2f805fbefd0ab3115acc3dfc9eece58b0ef70de
A.Klein <allenkleined@gmail.com>
2022-10-27 22:41:30 -0700

    added html route for notes and starter code

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const path=require('path');
+const app = express();
+const PORT = process.env.port || 3001;
+const data = require('./db/db.json');
+const uuid = require('./helper/uuid');
+const fs = require('fs');
+//html routes to send for 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/notes',(req,res)=>{
+  res.sendFile(path.join(__dirname,'/public/notes.html'));
+});
+//api routes
+
+
