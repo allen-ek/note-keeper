466ef9a1956c58cb1bbe849b5167d805303a1d97
A.Klein <allenkleined@gmail.com>
2022-10-28 20:34:09 -0700

    added post route and id npm

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express=require('express');
 const path=require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const data = require('./db/db.json');
-const uuid = require('./helper/uuid');
+let noteDB = require('./db/db.json');
+const {v4 : uuidv4} = require('uuid');
 const fs = require('fs');
 //html routes to send for 
 app.use(express.json());
@@ -16,16 +16,46 @@ app.get('/notes',(req,res)=>{
 //api routes
 
 app.get('/api/notes',(req,res)=>{
-  res.json(data);
+  res.json(noteDB);
 
   // Log our request to the terminal
-  console.info(data);
+  console.info(noteDB);
 });
 
 app.post("/api/notes/", function(req,res) {
   //logic to post a note
-  res.json(true);
+  const { title,text } = req.body;
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(), 
+    };
+    //const noteString=JSON.stringify(newNote);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        var noteString=JSON.parse(data);
+        noteString.push(newNote);
+        noteDB = noteString;
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(noteString),
+          (err) =>
+            err
+              ? console.error(err)
+              : console.info('Added notes')
+        );
+      }
+    });
+        
+
+  }//ifstatement 
+  res.json(newNote)
 });
+
 //catch all path
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'/public/index.html'));
